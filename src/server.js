6fa7e729a5df876d5af66edc912266df93652933
Gabriel Moreno <gabmormed@gmail.com>
2024-08-21 19:56:37 -0300

    subindo aula da semana passada

--- src/server.js
+++ src/server.js
@@ -1,21 +1,15 @@
-import express from "express"
-import userRouter from "./routers/userRouter.js"
-import prodsRouter from "./routers/prodsRouter.js"
+import express from 'express'
+import authRouter from './routers/authRouter.js'
+import accountRouter from './routers/accountRouter.js'
+import { ENVIRONMENT, PORT, HOST } from './config.js'
 
 const app = express()
-const port = 3000
 
-app.use('/user', userRouter)
-app.use('/prods', prodsRouter)
+app.get('/', (req, res) => {res.json({message: "Bem-vindo a API!"})})
 
-app.get('/', (req, res) => {
-  res.send('Olá Mundo!')
-})
+app.use('/auth', authRouter)
+app.use('/account', accountRouter)
 
-app.post('/', (req, res) => {
-    res.send('Olá Mundo!')
-  })
-
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Servidor Rodando no ambiente ${ENVIRONMENT} em ${ ENVIRONMENT == 'production' ? HOST : HOST+':'+PORT }`)
 })
